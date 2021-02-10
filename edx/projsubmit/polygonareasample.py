cc54be781af5c8d21752370d9ba7d56055bce983
gitoffmalawn <gitoffmylawn@gmail.com>
2021-02-09 22:51:49 -0700

    adding proj

---
+++ edx/projsubmit/polygonareasample.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 23:28:21 2021
+
+@author: fernando
+"""
+
+from math import tan, pi
+n_sides = int(input("Input number of sides: "))
+s_length = float(input("Input the length of a side: "))
+p_area = n_sides * (s_length ** 2) / (4 * tan(pi / n_sides))
+print("The area of the polygon is: ",p_area)
